7846759fb86082b9d4685041539cc912cc9b5fd1
PhatBaka <nguyenquyphat2711@gmail.com>
2023-05-31 08:33:37 +0700

    dev: fix config

--- src/config.ts
+++ src/config.ts
@@ -14,13 +14,13 @@ export const firebaseConfig = {
   // messagingSenderId: '71002133649',
   // appId: '1:71002133649:web:1f8ca95c618519df339b30',
   // measurementId: 'G-YPFBPWMLF6'
-  apiKey: "AIzaSyBLBV7jmbyQMWiXpY4Id5Lt-LRnWgTNETw",
-  authDomain: "krowdinvestment.firebaseapp.com",
-  projectId: "krowdinvestment",
-  storageBucket: "krowdinvestment.appspot.com",
-  messagingSenderId: "542746657150",
-  appId: "1:542746657150:web:aa01289296ead53820f32f",
-  measurementId: "G-ZZVEBWVHXS"
+  apiKey: 'AIzaSyBLBV7jmbyQMWiXpY4Id5Lt-LRnWgTNETw',
+  authDomain: 'krowdinvestment.firebaseapp.com',
+  projectId: 'krowdinvestment',
+  storageBucket: 'krowdinvestment.appspot.com',
+  messagingSenderId: '542746657150',
+  appId: '1:542746657150:web:aa01289296ead53820f32f',
+  measurementId: 'G-ZZVEBWVHXS'
 };
 
 export const cognitoConfig = {
